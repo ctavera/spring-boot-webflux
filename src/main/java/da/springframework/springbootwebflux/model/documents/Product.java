9a84ed50cd6a0944abf000a7557c116ffc0a6b28
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-07-15 12:03:50 -0500

    30. Add Document Model and Repository Clases. Closes #1.

---
+++ src/main/java/da/springframework/springbootwebflux/model/documents/Product.java
@@ -0,0 +1,52 @@
+package da.springframework.springbootwebflux.model.documents;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document(collection = "products")
+public class Product {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private Double price;
+
+    private Date creationDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+}
