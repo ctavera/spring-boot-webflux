807316da11d002a34e838ec33e836df7d3f10ee9
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-07-15 15:53:25 -0500

    35. Subscribe another Observable and modify the reactive stream. Closes #6.

--- src/main/java/da/springframework/springbootwebflux/controllers/ProductController.java
+++ src/main/java/da/springframework/springbootwebflux/controllers/ProductController.java
@@ -3,11 +3,13 @@ package da.springframework.springbootwebflux.controllers;
 import da.springframework.springbootwebflux.model.documents.Product;
 import da.springframework.springbootwebflux.repositories.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import reactor.core.publisher.Flux;
 
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class ProductController {
@@ -17,7 +19,15 @@ public class ProductController {
     @GetMapping({"/list", "/"})
     public String list (Model model) {
 
-        Flux<Product> productFlux = productRepository.findAll();
+        Flux<Product> productFlux = productRepository.findAll()
+                .map(product -> {
+                    product.setName(product.getName().toUpperCase());
+
+                    return product;
+                });
+
+        productFlux.subscribe(product -> log.info(product.getName()));
+
         model.addAttribute("products", productFlux);
         model.addAttribute("title", "Listado de Productos");
 
